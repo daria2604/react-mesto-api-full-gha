954a619c917199e424720bcc3692d6d01831760d
Daria Rutts <darya.rutts@yandex.ru>
2023-08-06 15:17:41 +0300

    fix: изменить порт

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,11 @@ const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.en
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:3000' ,'https://mesto.dvr.nomoreparties.co'],
+  origin: [
+    'http://localhost:3001',
+    'http://mesto.dvr.nomoreparties.co',
+    'https://mesto.dvr.nomoreparties.co'
+  ],
   credentials: true,
 }))
 app.use(requestLogger)
