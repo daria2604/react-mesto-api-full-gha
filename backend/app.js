48504e338bfb7fe0414eb284c416d15a74fe2799
Daria Rutts <darya.rutts@yandex.ru>
2023-08-06 15:04:23 +0300

    fix: добавить домен в настройки cors

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.en
 const app = express();
 
 app.use(cors({
-  origin: 'https://mesto.dvr.nomoreparties.co',
+  origin: ['http://localhost:3000' ,'https://mesto.dvr.nomoreparties.co'],
   credentials: true,
 }))
 app.use(requestLogger)
